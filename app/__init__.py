5573775761eeec68ec37617f192a58a9695de130
Mo_s <sun_hy@foxmail.com>
2017-12-31 11:41:32 +0800

    add auth buleprint url route

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
+from .main import main as main_blueprint
+from .auth import auth as auth_blueprint
+
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
@@ -21,8 +24,8 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
 
-    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
 
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -16,20 +16,20 @@ class UserModelTestCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_password_setter(self):
-        u = User(password='cat')
+        u = User(password='sun')
         self.assertTrue(u.password_hash is not None)
 
     def test_no_password_getter(self):
-        u = User(password='cat')
+        u = User(password='sun')
         with self.assertRaises(AttributeError):
             u.password
 
     def test_password_verification(self):
-        u = User(password='cat')
-        self.assertTrue(u.verify_password('cat'))
-        self.assertFalse(u.verify_password('dog'))
+        u = User(password='sun')
+        self.assertTrue(u.verify_password('sun'))
+        self.assertFalse(u.verify_password('mou'))
 
     def test_password_salts_are_random(self):
-        u = User(password='cat')
-        u2 = User(password='cat')
+        u = User(password='sun')
+        u2 = User(password='sun')
         self.assertTrue(u.password_hash != u2.password_hash)
