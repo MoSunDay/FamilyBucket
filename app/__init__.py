49154028c9f780a965ba23d2472601ef97ff24e7
Mo_s <sun_hy@foxmail.com>
2017-12-31 12:18:46 +0800

    add login buleprint

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
 
-from .main import main as main_blueprint
-from .auth import auth as auth_blueprint
-
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
@@ -16,8 +13,6 @@ db = SQLAlchemy()
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
-login_manager.session_protection = 'strong'
-
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -28,9 +23,12 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
-
