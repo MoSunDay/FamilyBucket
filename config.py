a16ca1b4eacb58d03f1f7668118b568ba72bf6fb
Mo_s <mo_s@foxmail.com>
2018-01-06 16:36:29 +0800

    change admin email

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ class Config:
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = 'mini'
-    FLASKY_MAIL_SENDER = MAIL_USERNAME
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    MININ_MAIL_SUBJECT_PREFIX = 'mini'
+    MININ_MAIL_SENDER = MAIL_USERNAME
+    MININ_ADMIN = os.environ.get('MINI_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
