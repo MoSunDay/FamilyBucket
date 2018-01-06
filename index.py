2ff580f4f4bd825c66d9abe05cdde1c12aaa1935
Mo_s <mo_s@foxmail.com>
2018-01-06 17:52:46 +0800

    found bpython context_processor bug

--- index.py
+++ index.py
@@ -12,7 +12,7 @@ manager = Manager(app)
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
-manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("shell", Shell(make_context=make_shell_context, use_bpython=False))
 manager.add_command("db", MigrateCommand)
 
 
