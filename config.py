f601d8d91788d27758af32b7a7f6f7dec53487ac
Mo_s <mo_s@foxmail.com>
2018-03-09 18:57:13 +0800

    fix some bug for comments and add moderate comments

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MINI_POSTS_PER_PAGE = 20
     MINI_FOLLOWERS_PER_PAGE = 50
+    MINI_COMMENTS_PER_PAGE = 30
 
     @staticmethod
     def init_app(app):
