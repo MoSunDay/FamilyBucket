c7fa6d3006be99a5e1d3f5f18ec6e2767156471a
Mo_s <mo_s@foxmail.com>
2018-03-09 19:37:37 +0800

    add Result Api for app

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,7 @@ pagedown = PageDown()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
@@ -33,5 +34,8 @@ def create_app(config_name):
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
     return app
